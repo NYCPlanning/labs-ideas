9ab26112096f7851325939b521386eca22631252
Chris Whong <chris@whong.org>
2017-07-20 11:19:16 -0400

    make eslint happy

--- src/IdeaCreate.js
+++ src/IdeaCreate.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './IdeaCreate.css';
 
-const IdeaCreate = (props) => {
+const IdeaCreate = () => (
+  <div className="project">
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+    <iframe
+      className="airtable-embed airtable-dynamic-height"
+      title="create-idea-form"
+      src="https://airtable.com/embed/shrbPCJw2eEGtDpyN?backgroundColor=yellow"
+      frameBorder="0"
+      width="100%"
+      height="1626"
+    />
+  </div>
+);
 
-  return (
-    <div className="project">
-    	<script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" title="create-idea-form" src="https://airtable.com/embed/shrbPCJw2eEGtDpyN?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="1626"></iframe>
-    </div>
-  );
-};
 
 export default IdeaCreate;
