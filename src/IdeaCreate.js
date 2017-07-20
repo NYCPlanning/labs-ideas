ebf481f09e9e89971e1f4bb26b99339da2e885eb
Matt Gardner <wilburnforce@gmail.com>
2017-07-20 11:10:57 -0400

    Create IdeaCreate component
    Embed Airtable code, with some modifications (to make React happy).
    
    Note: any time we update to include new Airtable embed, we must add a title to the iFrame and remove the style attribute.

---
+++ src/IdeaCreate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './IdeaCreate.css';
+
+const IdeaCreate = (props) => {
+
+  return (
+    <div className="project">
+    	<script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" title="create-idea-form" src="https://airtable.com/embed/shrbPCJw2eEGtDpyN?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="1626"></iframe>
+    </div>
+  );
+};
+
+export default IdeaCreate;
