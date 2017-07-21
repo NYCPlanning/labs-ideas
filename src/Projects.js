b37f968814dc555583b16bb817c8a7063d79ef4b
Andy Cochran <acochran@planning.nyc.gov>
2017-07-21 10:49:58 -0400

    style the grid view of ideas

--- src/Projects.js
+++ src/Projects.js
@@ -10,25 +10,33 @@ class Projects extends Component {
     const getProjects = () => {
       const { projects } = this.props;
       return projects.map(d => (
-        <List.Item key={d.project_id} >
-          <Icon name="idea" />
-          <List.Content>
-            <Link to={`/${d.slug}`}>
-              { d.project_name } - { d.point_of_contact }
-            </Link>
-          </List.Content>
-        </List.Item>
+        <div key={d.project_id} className="cell">
+          <div className="card">
+            <div className="card-section">
+              <h3>
+                <Link to={`/${d.slug}`}>
+                  { d.project_name }
+                </Link>
+              </h3>
+              <h4 className="header-small">{ d.division }</h4>
+              <p>{ d.short_description }</p>
+              <span className="label neighborhood-improvement">neighborhood-improvement</span>
+              <span className="label housing">housing</span>
+              <span className="label economic-development">economic-development</span>
+              <span className="label resiliency-and-sustainability">resiliency and sustainability</span>
+              <span className="label data-and-expertise">data-and-expertise</span>
+            </div>
+          </div>
+        </div>
       ));
     };
 
     return (
-      <List
-        size="huge"
-        divided
-        celled
-      >
-        { getProjects() }
-      </List>
+      <div className="grid-container">
+        <div className="grid-x grid-padding-x grid-padding-y medium-up-2">
+          { getProjects() }
+        </div>
+      </div>
     );
   }
 }
