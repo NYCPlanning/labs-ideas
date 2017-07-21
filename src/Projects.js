ac6013b6c2979f9179dee6dbeb17665eb11f5f16
Andy Cochran <acochran@planning.nyc.gov>
2017-07-21 10:54:03 -0400

    nix semantic-ui from projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { List, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import './Projects.scss';
