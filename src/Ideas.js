771a2f8299bae83bd4ba1728e27ce6b49b65703d
Chris Whong <chris@whong.org>
2017-07-21 14:08:28 -0400

    add spinkit css

--- src/Ideas.js
+++ src/Ideas.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import './Ideas.scss';
+import './Ideas.css';
 
 class Ideas extends Component {
   render() {
