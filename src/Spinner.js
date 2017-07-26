eb7d0905886c30f89d8524e6f147e5de18aee87e
Matt Gardner <wilburnforce@gmail.com>
2017-07-26 09:28:34 -0400

    Pass state as props to Spinner
    Spinner is either shown entirely or can be hidden internally. It must be able to handle show/hide logic internally because its parent, in this case IdeaCreate, may need to avoid re-rendering because its listening for an event emitted by an element (an iFrame). That event would endlessly be restarted otherwise.

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Spinner.css';
 
-const NotFound = () => (
-  <div className="grid-container">
-    <div className="grid-x grid-padding-x">
-      <div
-        className="cell large-text-center"
-        style={{ padding: '100px 0' }}
-      >
-        <div className="spinner">
-          <div className="bounce1" />
-          <div className="bounce2" />
-          <div className="bounce3" />
+const Spinner = props => (
+  <div>
+    { props.loading &&
+      (
+        <div className="grid-container">
+          <div className="grid-x grid-padding-x">
+            <div
+              className="cell large-text-center"
+              style={{ padding: '100px 0' }}
+            >
+              <div className="spinner">
+                <div className="bounce1" />
+                <div className="bounce2" />
+                <div className="bounce3" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )
+    };
   </div>
 );
 
-export default NotFound;
+Spinner.propTypes = {
+  loading: PropTypes.bool,
+};
+
+Spinner.defaultProps = {
+  loading: true,
+};
+
+export default Spinner;
